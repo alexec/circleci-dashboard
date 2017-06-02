3361543ea2f784df5c0358327bbf7475d0f1d6c6
Alex Collins <Alex_Collins@intuit.coml>
2017-06-02 17:30:50 +0100

    rubbish

---
+++ src/main/java/dashboard/App.java
@@ -0,0 +1,13 @@
+package dashboard;
+
+import io.swagger.annotations.Api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+
+    }
+}
